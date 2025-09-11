6533a6a48e48d3d21ecb9e56b52fccdb13f7a8bc
chibuzo <uchibuzo1@gmail.com>
2025-09-11 22:33:21 +0100

    quiz

---
+++ Quiz/quiz.js
@@ -0,0 +1,23 @@
+const Questions = [
+  {
+    question: "Who is the best footballer ?",
+    options: ["Messi", "Ronaldo", "Obinna", "Otti"],
+    correct: 0,
+  },
+  {
+    question: "What is 1+1 ?",
+    options: ["1", "10", "2", "12"],
+    correct: 2,
+  },
+  {
+    question: "Who is the President of Nigeria ?",
+    options: ["Atiku", "Tinubu", "Obi", "Ernest"],
+    correct: 1,
+  },
+  {
+    question: "Who is the  Governor of Abia?",
+    options: ["Fola", "Ronaldo", "Obinna", "Otti"],
+    correct: 3,
+  },
+];
+const displayQuestion = document.querySelector("#displayQuestion");
